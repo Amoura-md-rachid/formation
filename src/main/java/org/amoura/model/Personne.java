af07699a07a6e248753ecd3a7a833bda0c2ba0b7
Amoura-md-rachid <mohand-rachid.amoura@cmconsulting-dz.com>
2023-12-10 17:27:58 +0100

    ajouter la classe Personne

---
+++ src/main/java/org/amoura/model/Personne.java
@@ -0,0 +1,54 @@
+package org.amoura.model;
+
+
+import java.util.Objects;
+
+public class Personne {
+
+    private String nom;
+    private int age;
+
+    public Personne(String nom, int age) {
+        this.nom = nom;
+        this.age = age;
+    }
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Personne{" +
+                "nom='" + nom + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Personne personne = (Personne) o;
+        return age == personne.age && Objects.equals(nom, personne.nom);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nom, age);
+    }
+}
+
